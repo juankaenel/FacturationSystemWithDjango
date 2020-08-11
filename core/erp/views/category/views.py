c9619642d99f1fc448a091ccd567ead41bdea462
Juan <kaenel10@gmail.com>
2020-08-11 11:42:33 -0300

    Implementing CategoryUpdateView

--- core/erp/views/category/views.py
+++ core/erp/views/category/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView   #Vistas genéricas
+#erp
 from core.erp.forms import CategoryForm
 from core.erp.models import Category
 
@@ -74,3 +75,34 @@ class CategoryCreateView(CreateView):
         context['action'] = 'add' #action la usamos para verificar el tipo de acción se quiere realizar
         return context
 
+class CategoryUpdateView(UpdateView):
+    model = Category
+    form_class = CategoryForm  # le paso el formulario basado en clases
+    template_name = 'category/create.html'  # le paso el template
+    success_url = reverse_lazy('erp:category_list')
+
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().dispatch(request,*args,**kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'edit':
+                form = self.get_form() #form = CategoryForm(request.POST)
+                data = form.save() #llamo al metodo save de forms.py de category
+            else:
+                data['error'] = 'No ha especificado ninguna acción'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data)
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] ='Edición de una categoría'
+        context['entity'] = 'Categorías'
+        context['list_url'] = reverse_lazy('erp:category_list')
+        context['action'] = 'edit' #action la usamos para verificar el tipo de acción se quiere realizar
+        return context
