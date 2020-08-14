7829f49ad98c0bc9a53af88e9c4cccc11f083f54
Juan <kaenel10@gmail.com>
2020-08-13 22:56:14 -0300

    Implementig ajax in datatables for category listing

---
+++ core/erp/static/category/js/list.js
@@ -0,0 +1,40 @@
+//esta funcion me permite trabajar con muchos datos en mis datatables
+$(function () {
+    $('#data').DataTable({
+        responsive:true,
+        autoWidth:false,
+        destroy:true,
+        deferRender:true, //se pone cuando hay datos q superen los 50000
+        ajax:{
+            url: window.location.pathname,
+            type:'POST',
+            data:{
+                'action':'datasearch' //esto se lee en el views de category list view como identificador
+            },
+            dataSrc:''
+        },
+        columns:[
+            {"data":"id"}, //0
+            {"data":"name"}, //1
+            {"data":"desc"}, //2
+            {"data":"desc"}  //3
+        ],
+        columnDefs:[ //personalizacion de las columnas
+            {
+                targets: [-1], //de atrás para adelante
+                class: 'text-center',
+                orderable:false,
+                render: function (data,type,row) { //desde acá le mandamos los botones y las rutas
+                    let buttons = '<a href="/erp/category/update/'+row.id+'" type="button" class="btn btn-warning btn-xs btn-flat"><i class="fas fa-edit"></i></a> ';
+                    buttons += '<a href="/erp/category/delete/'+row.id+'" type="button" class="btn btn-danger btn-xs btn-flat"><i class="fas fa-trash-alt"></i></a>';
+                    return buttons;
+                }
+            },
+        ],
+        initComplete:function (settings,json) { //se ejecuta cuando se carga la tabla
+            //alert('se cargó la tabla crack');
+        }
+
+    })
+
+})
--- core/erp/views/category/views.py
+++ core/erp/views/category/views.py
@@ -1,15 +1,16 @@
-#Django
+# Django
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView   #Vistas genéricas
-#erp
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView  # Vistas genéricas
+# erp
 from core.erp.forms import CategoryForm
 from core.erp.models import Category
 
-#Vista basada en función
+
+# Vista basada en función
 # def category_list(request):
 #     data = {
 #         'title' : 'Listado de categorías',
@@ -17,50 +18,70 @@ from core.erp.models import Category
 #     }
 #     return render(request,'category/list.html',data)
 
-#Vista basada en clase
+# Vista basada en clase
 class CategoryListView(ListView):
     model = Category
     template_name = 'category/list.html'
 
-    @method_decorator(csrf_exempt) #decorador para habilitar el crsf
-    #reescribimos el metodo dispatch
+    @method_decorator(csrf_exempt)  # decorador para habilitar el crsf
+    # reescribimos el metodo dispatch
     def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request,*args,**kwargs)
+        return super().dispatch(request, *args, **kwargs)
 
-    def post(self,request,*args,**kwargs):
+    # en caso de no usar muchos datos en tablas
+    # def post(self,request,*args,**kwargs):
+    #     data = {}
+    #     #print(request.POST)
+    #     try:
+    #         data = Category.objects.get(pk=request.POST['id']).toJson() #Si se comple llama al metodo toJson y me convierte los datos a diccinario
+    #     except Exception as e:
+    #         data['error'] = str(e)
+    #     return JsonResponse(data)
+
+    # en caso de usar muchos y que tarden mucho las peticiones vamos a hacer uso de ajax con datatables
+    def post(self, request, *args, **kwargs):
         data = {}
-        #print(request.POST)
         try:
-            data = Category.objects.get(pk=request.POST['id']).toJson() #Si se comple llama al metodo toJson y me convierte los datos a diccinario
+            action = request.POST['action']
+            if action == 'datasearch':
+                data = []
+                for i in Category.objects.all():
+                    data.append(i.toJson())  # AGREGO a la lista data todos los objetos de la categoria en forma de diccionario
+            else:
+                data['error'] = 'Ha ocurrido un error'
         except Exception as e:
-            data['error'] = str(e)
-        return JsonResponse(data)
+           data['error'] = str(e)
 
-    #sobreescribo el data a enviar debido a que se enviaba vacío
+        return JsonResponse(data, safe=False) #en este caso para serializar objeto que no están en formato diccionario hay que poner safe = False
+
+
+# sobreescribo el data a enviar debido a que se enviaba vacío
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] ='Listado de categorías'
-        context['entity'] = 'Categorías' #esto viaja al body para que tome ese nombre el href
+        context['title'] = 'Listado de categorías'
+        context['entity'] = 'Categorías'  # esto viaja al body para que tome ese nombre el href
         context['create_url'] = reverse_lazy('erp:category_create')
         context['list_url'] = reverse_lazy('erp:category_list')
         return context
 
+
 class CategoryCreateView(CreateView):
     model = Category
-    form_class = CategoryForm #le paso el formulario basado en clases
-    template_name = 'category/create.html' #le paso el template
-    success_url = reverse_lazy('erp:category_list') #una vez que se ejecute dicho formulario redireccioname a category list
+    form_class = CategoryForm  # le paso el formulario basado en clases
+    template_name = 'category/create.html'  # le paso el template
+    success_url = reverse_lazy(
+        'erp:category_list')  # una vez que se ejecute dicho formulario redireccioname a category list
 
     def post(self, request, *args, **kwargs):
         data = {}
         # print(request.POST)
         try:
-            #data = Category.objects.get(
+            # data = Category.objects.get(
             #    pk=request.POST['id']).toJson()  # Si se comple llama al metodo toJson y me convierte los datos a diccinario
             action = request.POST['action']
             if action == 'add':
-                form = self.get_form() #form = CategoryForm(request.POST)
-                data = form.save() #llamo al metodo save de forms.py de category
+                form = self.get_form()  # form = CategoryForm(request.POST)
+                data = form.save()  # llamo al metodo save de forms.py de category
             else:
                 data['error'] = 'No ha especificado ninguna acción'
         except Exception as e:
@@ -69,11 +90,12 @@ class CategoryCreateView(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] ='Crear categoría'
+        context['title'] = 'Crear categoría'
         context['entity'] = 'Categorías'
         context['list_url'] = reverse_lazy('erp:category_list')
-        context['action'] = 'add' #action la usamos para verificar el tipo de acción se quiere realizar
+        context['action'] = 'add'  # action la usamos para verificar el tipo de acción se quiere realizar
         return context
+
 
 class CategoryUpdateView(UpdateView):
     model = Category
@@ -83,42 +105,42 @@ class CategoryUpdateView(UpdateView):
 
     def dispatch(self, request, *args, **kwargs):
         self.object = self.get_object()
-        return super().dispatch(request,*args,**kwargs)
+        return super().dispatch(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         data = {}
         try:
             action = request.POST['action']
             if action == 'edit':
-                form = self.get_form() #form = CategoryForm(request.POST)
-                data = form.save() #llamo al metodo save de forms.py de category
+                form = self.get_form()  # form = CategoryForm(request.POST)
+                data = form.save()  # llamo al metodo save de forms.py de category
             else:
                 data['error'] = 'No ha especificado ninguna acción'
         except Exception as e:
             data['error'] = str(e)
         return JsonResponse(data)
 
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] ='Edición de una categoría'
+        context['title'] = 'Edición de una categoría'
         context['entity'] = 'Categorías'
         context['list_url'] = reverse_lazy('erp:category_list')
-        context['action'] = 'edit' #action la usamos para verificar el tipo de acción se quiere realizar
+        context['action'] = 'edit'  # action la usamos para verificar el tipo de acción se quiere realizar
         return context
+
 
 class CategoryDeleteView(DeleteView):
     model = Category
     template_name = 'category/delete.html'  # le paso el template
     success_url = reverse_lazy('erp:category_list')
 
-    #method_decorator(csrf_exempt) #con esto me evito enviarle parametros en ajax a través del data
+    # method_decorator(csrf_exempt) #con esto me evito enviarle parametros en ajax a través del data
     def dispatch(self, request, *args, **kwargs):
-        self.object = self.get_object() #la asignamos para que pueda hacer uso en el metodo post
-        return super().dispatch(request,*args,**kwargs)
+        self.object = self.get_object()  # la asignamos para que pueda hacer uso en el metodo post
+        return super().dispatch(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        data={}
+        data = {}
         try:
             self.object.delete()
         except Exception as e:
@@ -127,8 +149,8 @@ class CategoryDeleteView(DeleteView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] ='Eliminación de una categoría'
+        context['title'] = 'Eliminación de una categoría'
         context['entity'] = 'Categorías'
         context['list_url'] = reverse_lazy('erp:category_list')
-        context['action'] = 'edit' #action la usamos para verificar el tipo de acción se quiere realizar
+        context['action'] = 'edit'  # action la usamos para verificar el tipo de acción se quiere realizar
         return context
