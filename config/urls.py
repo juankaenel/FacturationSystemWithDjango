2a96beab68a117a4fd9a7be2896e352e75f961f9
Juan <kaenel10@gmail.com>
2020-08-17 19:10:34 -0300

    LoginFormView, other method for login

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from core.homepage.views import IndexView
 from core.login.views import LoginFormView
+#from core.login.views import LoginFormView2
 
 urlpatterns = [
     path('', IndexView.as_view()),
     path('login/', LoginFormView.as_view()),
+    #path('login2/', LoginFormView2.as_view()),
     path('admin/', admin.site.urls),
     path('erp/', include('core.erp.urls')),
 
--- core/login/views.py
+++ core/login/views.py
@@ -1,7 +1,18 @@
+#Primer Método
 from django.contrib.auth.views import LoginView #vista genérica login view
 from django.shortcuts import redirect
+#Segundo Método
+#from django.urls import reverse_lazy
+#from django.views.generic import FormView
+#from django.contrib.auth.forms import AuthenticationForm
+#from django.contrib.auth import login
+#from django.http import HttpResponseRedirect
 
+# Vamos a mostrar dos métodos de login, son parecidos ambos puedes sobreescribir el método form_valid puesto que ambos
+# implementan FormView la diferencia es que uno ya tiene el form y la validación del login.
+# Yo utilizo el LoginView porque ya esta hecho no me preocupo por lo demás.
 
+#Primer metodo de login  -> recomendable
 class LoginFormView(LoginView):
     """
     Vista basada en clase LoginView
@@ -19,3 +30,28 @@ class LoginFormView(LoginView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Iniciar Sesión'
         return context
+
+#Segundo método de login
+# class LoginFormView2(FormView):
+#     """
+#     Vista genérica LoginFormView que hereda de  Form view
+#     """
+#     template_name = 'login.html'
+#     form_class = AuthenticationForm
+#     success_url = reverse_lazy('erp:category_list')
+#
+#
+#     def dispatch(self, request, *args, **kwargs):
+#         #el usuario lo sacamos a través del request
+#         if request.user.is_authenticated: #si se encuentra logeado
+#             return HttpResponseRedirect(self.success_url)
+#         return super().dispatch(request, *args, **kwargs)
+#
+#     def form_valid(self, form):
+#         login(self.request,form.get_user())
+#         return HttpResponseRedirect(self.success_url)
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['title'] = 'Iniciar Sesión'
+#         return context
